f8bed4be33a5335381844ee880c6a5f7d372fc38
Pavsheto <pavsheto@gmail.com>
2025-05-21 10:03:54 +0300

    add Print Even Numbers

---
+++ PrintEvenNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PrintEvenNumbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+        Queue<int> queue = new Queue<int>(numbers);
+
+        List<int> evenNumbers = new List<int>();
+
+        while (queue.Count > 0)
+        {
+            int current = queue.Dequeue();
+            if (current % 2 == 0)
+            {
+                evenNumbers.Add(current);
+            }
+        }
+        
+        Console.WriteLine(string.Join(", ", evenNumbers));
+    }
+}
