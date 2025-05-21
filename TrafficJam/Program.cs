23990674627228f6915e0546bf2f5c8b4b9484cc
Pavsheto <pavsheto@gmail.com>
2025-05-21 10:11:15 +0300

    add

--- TrafficJam/Program.cs
+++ TrafficJam/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int carsPerGreen = int.Parse(Console.ReadLine()); 
+        Queue<string> queue = new Queue<string>();      
+        int totalPassed = 0;
+
+        string input;
+        while ((input = Console.ReadLine()) != "end")
+        {
+            if (input == "green")
+            {
+                for (int i = 0; i < carsPerGreen && queue.Count > 0; i++)
+                {
+                    string car = queue.Dequeue();
+                    Console.WriteLine($"{car} passed!");
+                    totalPassed++;
+                }
+            }
+            else
+            {
+                queue.Enqueue(input);
+            }
+        }
+
+        Console.WriteLine($"{totalPassed} cars passed the crossroads.");
     }
 }
