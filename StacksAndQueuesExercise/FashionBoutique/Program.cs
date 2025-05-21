332ab0ab6575c28550aec47dfbb7b821ec695be6
Pavsheto <pavsheto@gmail.com>
2025-05-21 10:27:14 +0300

    add Fashion Boutique

---
+++ StacksAndQueuesExercise/FashionBoutique/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace FashionBoutique;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stack<int> clothes = new Stack<int>(
+            Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+        );
+
+        int rackCapacity = int.Parse(Console.ReadLine());
+
+        int racksUsed = 1;
+        int currentSum = 0;
+
+        while (clothes.Count > 0)
+        {
+            int cloth = clothes.Peek();
+
+            if (currentSum + cloth == rackCapacity)
+            {
+                currentSum = 0;
+                racksUsed++;
+                clothes.Pop();
+            }
+            else if (currentSum + cloth < rackCapacity)
+            {
+                currentSum += cloth;
+                clothes.Pop();
+            }
+            else
+            {
+                racksUsed++;
+                currentSum = 0;
+            }
+        }
+
+        Console.WriteLine(racksUsed);
+    }
+}
