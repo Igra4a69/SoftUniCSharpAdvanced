681a4e8233e77521a1bfe3f57b98fce2a314f57e
Pavsheto <pavsheto@gmail.com>
2025-05-21 17:00:39 +0300

    add Sum Matrix Elements

---
+++ MultidimensionalArraysLab/SumMatrixElements/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace SumMatrixElements;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] dimensions = Console.ReadLine()
+            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        int rows = dimensions[0];
+        int cols = dimensions[1];
+
+        int[,] matrix = new int[rows, cols];
+        int sum = 0;
+        
+        for (int row = 0; row < rows; row++)
+        {
+            int[] rowData = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int col = 0; col < cols; col++)
+            {
+                matrix[row, col] = rowData[col];
+                sum += matrix[row, col];
+            }
+        }
+
+        Console.WriteLine(rows);
+        Console.WriteLine(cols);
+        Console.WriteLine(sum);
+    }
+}
