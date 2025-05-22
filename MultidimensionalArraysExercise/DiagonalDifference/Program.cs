d22a4a0d4bb434efa610408391e452bfca1a791e
Pavsheto <pavsheto@gmail.com>
2025-05-22 18:33:24 +0300

    add  Diagonal Difference

---
+++ MultidimensionalArraysExercise/DiagonalDifference/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace DiagonalDifference;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int[,] matrix = new int[n, n];
+
+        for (int row = 0; row < n; row++)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            for (int col = 0; col < n; col++)
+            {
+                matrix[row, col] = int.Parse(input[col]);
+            }
+        }
+
+        int primaryDiagonalSum = 0;
+        int secondaryDiagonalSum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            primaryDiagonalSum += matrix[i, i];               
+            secondaryDiagonalSum += matrix[i, n - 1 - i];     
+        }
+
+        int difference = Math.Abs(primaryDiagonalSum - secondaryDiagonalSum);
+        Console.WriteLine(difference);
+    }
+}
