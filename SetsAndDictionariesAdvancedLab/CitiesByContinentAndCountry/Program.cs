4bc587edba10020de568ec8078d6ed58362763dd
Pavsheto <pavsheto@gmail.com>
2025-05-23 15:13:52 +0300

    add Cities by Continent and Country

---
+++ SetsAndDictionariesAdvancedLab/CitiesByContinentAndCountry/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace CitiesByContinentAndCountry;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Dictionary<string, Dictionary<string, List<string>>> data =
+            new Dictionary<string, Dictionary<string, List<string>>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] parts = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string continent = parts[0];
+            string country = parts[1];
+            string city = parts[2];
+
+            if (!data.ContainsKey(continent))
+            {
+                data[continent] = new Dictionary<string, List<string>>();
+            }
+
+            if (!data[continent].ContainsKey(country))
+            {
+                data[continent][country] = new List<string>();
+            }
+
+            data[continent][country].Add(city);
+        }
+
+        foreach (KeyValuePair<string, Dictionary<string, List<string>>> continentEntry in data)
+        {
+            Console.WriteLine($"{continentEntry.Key}:");
+            foreach (KeyValuePair<string, List<string>> countryEntry in continentEntry.Value)
+            {
+                string cities = string.Join(", ", countryEntry.Value);
+                Console.WriteLine($"  {countryEntry.Key} -> {cities}");
+            }
+        }
+    }
+}
