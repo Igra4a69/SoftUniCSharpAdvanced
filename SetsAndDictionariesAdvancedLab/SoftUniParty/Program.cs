8c254ab75234f9846c46178d20dc3390ee5f802a
Pavsheto <pavsheto@gmail.com>
2025-05-23 15:16:56 +0300

    add SoftUni Party

---
+++ SetsAndDictionariesAdvancedLab/SoftUniParty/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace SoftUniParty;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<string> vipGuests = new HashSet<string>();
+        HashSet<string> regularGuests = new HashSet<string>();
+
+        string input = Console.ReadLine();
+
+        while (input != "PARTY")
+        {
+            if (char.IsDigit(input[0]))
+            {
+                vipGuests.Add(input);
+            }
+            else
+            {
+                regularGuests.Add(input);
+            }
+
+            input = Console.ReadLine();
+        }
+
+        input = Console.ReadLine();
+
+        while (input != "END")
+        {
+            if (vipGuests.Contains(input))
+            {
+                vipGuests.Remove(input);
+            }
+            else if (regularGuests.Contains(input))
+            {
+                regularGuests.Remove(input);
+            }
+
+            input = Console.ReadLine();
+        }
+
+        Console.WriteLine(vipGuests.Count + regularGuests.Count);
+
+        foreach (string guest in vipGuests)
+        {
+            Console.WriteLine(guest);
+        }
+
+        foreach (string guest in regularGuests)
+        {
+            Console.WriteLine(guest);
+        }
+    }
+}
