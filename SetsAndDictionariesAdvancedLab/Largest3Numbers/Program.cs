1e8c4e89fd33e8f33ac952e374d1bec38e612087
Pavsheto <pavsheto@gmail.com>
2025-05-23 15:11:20 +0300

    add Largest 3 Numbers

---
+++ SetsAndDictionariesAdvancedLab/Largest3Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Largest3Numbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        int[] sortedDescending = numbers
+            .OrderByDescending(x => x)
+            .ToArray();
+
+        int count = Math.Min(3, sortedDescending.Length);
+
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write($"{sortedDescending[i]} ");
+        }
+    }
+}
