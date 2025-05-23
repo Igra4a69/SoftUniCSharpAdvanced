2c87e7b912d9655e03b7e9870dca35331823ca28
Pavsheto <pavsheto@gmail.com>
2025-05-23 15:12:25 +0300

    add Product Shop

---
+++ SetsAndDictionariesAdvancedLab/ProductShop/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ProductShop;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SortedDictionary<string, Dictionary<string, double>> shops =
+            new SortedDictionary<string, Dictionary<string, double>>();
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input == "Revision")
+            {
+                break;
+            }
+
+            string[] parts = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            string shop = parts[0];
+            string product = parts[1];
+            double price = double.Parse(parts[2]);
+
+            if (!shops.ContainsKey(shop))
+            {
+                shops[shop] = new Dictionary<string, double>();
+            }
+
+            shops[shop][product] = price;
+        }
+
+        foreach (KeyValuePair<string, Dictionary<string, double>> shopEntry in shops)
+        {
+            Console.WriteLine($"{shopEntry.Key}->");
+            foreach (KeyValuePair<string, double> productEntry in shopEntry.Value)
+            {
+                Console.WriteLine($"Product: {productEntry.Key}, Price: {productEntry.Value}");
+            }
+        }
+    }
+}
