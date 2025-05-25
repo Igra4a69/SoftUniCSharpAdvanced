4b0a82174c4f38d1aa56c93697523281425f0cf3
Pavsheto <pavsheto@gmail.com>
2025-05-25 16:03:47 +0300

    add Even Times

---
+++ SetsAndDictionariesAdvancedExercise/EvenTimes/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace EvenTimes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Dictionary<int, int> counts = new Dictionary<int, int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (counts.ContainsKey(number))
+            {
+                counts[number]++;
+            }
+            else
+            {
+                counts[number] = 1;
+            }
+        }
+
+        foreach (KeyValuePair<int, int> pair in counts)
+        {
+            if (pair.Value % 2 == 0)
+            {
+                Console.WriteLine(pair.Key);
+                break;
+            }
+        }
+    }
+}
