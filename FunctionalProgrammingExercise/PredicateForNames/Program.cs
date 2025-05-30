5c7176a223e374b046c378f560ca57e3bdede542
Pavsheto <pavsheto@gmail.com>
2025-05-30 16:41:30 +0300

    add Predicate For Names

---
+++ FunctionalProgrammingExercise/PredicateForNames/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        string[] names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        Predicate<string> isShortEnough = name => name.Length <= n;
+
+        PrintFilteredNames(names, isShortEnough);
+    }
+
+    static void PrintFilteredNames(string[] names, Predicate<string> predicate)
+    {
+        for (int i = 0; i < names.Length; i++)
+        {
+            if (predicate(names[i]))
+            {
+                Console.WriteLine(names[i]);
+            }
+        }
+    }
+}
