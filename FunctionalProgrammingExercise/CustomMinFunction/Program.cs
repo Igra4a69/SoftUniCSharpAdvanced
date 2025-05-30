6e01911e8eece73e297caa556f248dc52b9bc83b
Pavsheto <pavsheto@gmail.com>
2025-05-30 16:29:08 +0300

    add Custom Min Function

---
+++ FunctionalProgrammingExercise/CustomMinFunction/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CustomMinFunction;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        Func<int[], int> findMin = nums =>
+        {
+            int min = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] < min)
+                    min = nums[i];
+            }
+            return min;
+        };
+
+        int minNumber = findMin(numbers);
+        Console.WriteLine(minNumber);
+    }
+}
