a9ea0db4fa48ab24a85f3cf1b099b31953689cb0
Pavsheto <pavsheto@gmail.com>
2025-05-30 16:14:08 +0300

    add Sort Even Numbers

---
+++ SortEvenNumbers/SortEvenNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SortEvenNumbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = Console.ReadLine()
+            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        int[] evenNumbers = numbers
+            .Where(n => n % 2 == 0)
+            .OrderBy(n => n)
+            .ToArray();
+
+        Console.WriteLine(string.Join(", ", evenNumbers));
+    }
+}
