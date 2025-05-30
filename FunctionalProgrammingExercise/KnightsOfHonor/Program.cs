6da5a1fbd06281fb0b10d4e45eb29411fc576802
Pavsheto <pavsheto@gmail.com>
2025-05-30 16:27:12 +0300

    add Knights of Honor

---
+++ FunctionalProgrammingExercise/KnightsOfHonor/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace KnightsOfHonor;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        Action<string> knight = name => Console.WriteLine("Sir " + name);
+
+        foreach (string name in names)
+        {
+            knight(name);
+        }
+    }
+}
