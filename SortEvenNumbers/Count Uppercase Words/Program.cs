ab499bfcd92c3a6d61fb8e136180be782474a017
Pavsheto <pavsheto@gmail.com>
2025-05-30 16:17:54 +0300

    add Count Uppercase Words

---
+++ SortEvenNumbers/Count Uppercase Words/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Count_Uppercase_Words;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+
+        Func<string, bool> isUppercase = word =>
+            !string.IsNullOrEmpty(word) && char.IsUpper(word[0]);
+
+        string[] words = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        IEnumerable<string> uppercaseWords = words.Where(isUppercase);
+
+        foreach (string word in uppercaseWords)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
