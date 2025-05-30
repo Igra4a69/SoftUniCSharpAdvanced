988661bc49003e6170e35ec0faf9d2573cd118b4
Pavsheto <pavsheto@gmail.com>
2025-05-30 16:42:44 +0300

    add List Of Predicates

---
+++ FunctionalProgrammingExercise/ListOfPredicates/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace ListOfPredicates;
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        string[] dividerStrings = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        int[] dividers = new int[dividerStrings.Length];
+        for (int i = 0; i < dividerStrings.Length; i++)
+        {
+            dividers[i] = int.Parse(dividerStrings[i]);
+        }
+
+        List<Predicate<int>> predicates = new List<Predicate<int>>();
+        for (int i = 0; i < dividers.Length; i++)
+        {
+            int currentDivider = dividers[i]; 
+            predicates.Add(number => number % currentDivider == 0);
+        }
+
+        List<int> divisibleNumbers = FindDivisibleNumbers(n, predicates);
+
+        Console.WriteLine(string.Join(' ', divisibleNumbers));
+    }
+
+    static List<int> FindDivisibleNumbers(int n, List<Predicate<int>> predicates)
+    {
+        List<int> result = new List<int>();
+
+        for (int number = 1; number <= n; number++)
+        {
+            bool isDivisibleByAll = true;
+            for (int i = 0; i < predicates.Count; i++)
+            {
+                if (!predicates[i](number))
+                {
+                    isDivisibleByAll = false;
+                    break;
+                }
+            }
+
+            if (isDivisibleByAll)
+            {
+                result.Add(number);
+            }
+        }
+
+        return result;
+    }
+}
