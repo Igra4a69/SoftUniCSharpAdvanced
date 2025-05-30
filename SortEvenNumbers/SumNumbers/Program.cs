4bf39cd515e01c96f7712750b22ebe6b3c345630
Pavsheto <pavsheto@gmail.com>
2025-05-30 16:16:02 +0300

    add Sum Numbers

---
+++ SortEvenNumbers/SumNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SumNumbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = Console.ReadLine()
+            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        Console.WriteLine(numbers.Count());
+        Console.WriteLine(numbers.Sum());
+    }
+}
